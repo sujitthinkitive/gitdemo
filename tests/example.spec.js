4e24838c6289524df99fd5cf7b609de503169af5
Hemendra Singh <hemendra.singh@thinkitive.com>
2025-07-24 19:38:21 +0530

    added new comment line

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -7,6 +7,7 @@ test('has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
   //this is a comment line
+  //this is a comment line hemendra
   //this is a comment line
 });
 
